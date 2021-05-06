86ddb08e9ababfa370715f21f7b84b503bbd0d2f
Jan Lishak <any@some.se>
2021-05-06 16:41:31 +0000

    watch logs scripts

--- src/main/java/dk/grinhouse/api/controllers/GreenhouseController.java
+++ src/main/java/dk/grinhouse/api/controllers/GreenhouseController.java
@@ -21,21 +21,21 @@ public class GreenhouseController
     this.greenhouseService = greenhouseService;
   }
 
-  @Operation(summary = "login | provide credentials to get greenhouse information", description = "returns information about greenhouse such as id and name")
+  @Operation(summary = "login, provide credentials to get greenhouse information", description = "returns information about greenhouse such as id and name")
   @PostMapping("/api/greenhouse")
   public Greenhouse login(@RequestBody Credentials credentials)
   {
     return greenhouseService.getGreenhouse(credentials);
   }
 
-  @Operation(summary = "[debug only] | allows you to create a greenhouse", description = "greenhouse can be created using this endpoint for debug purposes")
+  @Operation(summary = "[debug only] allows you to create a greenhouse", description = "greenhouse can be created using this endpoint for debug purposes")
   @PutMapping("/api/greenhouse")
   public void create(@RequestBody Greenhouse greenhouse)
   {
     greenhouseService.createGreenhouse(greenhouse);
   }
 
-  @Operation(summary = "[debug only] | get list of all the greenhouses in the database", description = "returns a list of all greenhouses")
+  @Operation(summary = "[debug only] get list of all the greenhouses in the database", description = "returns a list of all greenhouses")
   @GetMapping("/api/greenhouse")
   public List<Greenhouse> getAll()
   {
