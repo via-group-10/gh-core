9924e8a99c76cc9dab15c16326908f55f546285b
Jan Lishak <35602700+janlishak@users.noreply.github.com>
2021-05-13 16:28:46 +0200

    moving to cloud

--- server/ubuntu-install-webhook-docker.sh
+++ server/ubuntu-install-webhook-docker.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-sudo su
 apt update
-apt install curl webhook
+apt install curl webhook docker-compose openjdk-11-jre-headless
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
