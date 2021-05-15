3005867c4e3fcd13e9a9733d83d64fed8bd408b2
Jan Lishak <35602700+janlishak@users.noreply.github.com>
2021-05-15 16:30:10 +0200

    change in queary

--- src/main/java/dk/grinhouse/persistence/repositories/IMeasurementRepository.java
+++ src/main/java/dk/grinhouse/persistence/repositories/IMeasurementRepository.java
@@ -7,14 +7,12 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.sql.Time;
-import java.sql.Timestamp;
 import java.util.List;
 
 @Repository
 public interface IMeasurementRepository extends JpaRepository<Measurement, Integer>
 {
-  @Query(value = "select measurementId, measuredValue, measurementDateTime, belongsTo, a.isOfType from GrinHouse.stage.measurement a inner join ( select isOfType, max(measurementDateTime) as MaxDate from GrinHouse.stage.measurement group by isOfType ) b on a.isOfType = b.isOfType and a.measurementDateTime = b.MaxDate", nativeQuery = true)
+  @Query(value = "select measurementId, measuredValue, measurementDateTime, belongsTo, a.isOfType from GrinHouse.dbo.Measurement a inner join ( select isOfType, max(measurementDateTime) as MaxDate from GrinHouse.dbo.Measurement group by isOfType ) b on a.isOfType = b.isOfType and a.measurementDateTime = b.MaxDate", nativeQuery = true)
   List<Measurement> getLatestMeasurements();
   List<Measurement> findByMeasurementTypeEnum(MeasurementTypeEnum measurementTypeEnum);
 
